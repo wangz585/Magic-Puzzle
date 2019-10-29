4196c8b90c7545258591b48ef057fb5895db73f8
helenzzq <helenzhu0411@gmail.com>
2019-10-29 13:52:37 -0400

    Add the nickname input box in register acitivity and update its xml

--- phase1/app/src/main/java/com/example/mainactivity/RegisterActivity.java
+++ phase1/app/src/main/java/com/example/mainactivity/RegisterActivity.java
@@ -23,14 +23,13 @@ public class RegisterActivity extends AppCompatActivity {
      * A UserManager
      */
     private UserManager usermanager;
-    RadioGroup targets = findViewById(R.id.radio);
+    RadioGroup target = findViewById(R.id.radio);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         usermanager = LoginActivity.userManager;
         setRegisterListener();
-        targets.setOnCheckedChangeListener(new Radio_check());
     }
 
 
@@ -40,8 +39,8 @@ public class RegisterActivity extends AppCompatActivity {
     class Radio_check implements  RadioGroup.OnCheckedChangeListener
     {          @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
-        View radioButton = targets.findViewById(checkedId);
-        int index = targets.indexOfChild(radioButton);
+        View radioButton = target.findViewById(checkedId);
+        int index = target.indexOfChild(radioButton);
         switch (index) {
             case 0:
                 usermanager.getActiveUser.setTarget(findViewById(R.id.kings));
@@ -59,7 +58,7 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     /**
-     * Activate the Register button.
+     * Activate the Register button. Store relevant data of user
      */
     @SuppressLint("SetTextI18n")
     public void setRegisterListener() {
@@ -67,10 +66,12 @@ public class RegisterActivity extends AppCompatActivity {
         signUp.setOnClickListener((v) -> {
             EditText username = findViewById(R.id.username);
             EditText password = findViewById(R.id.password);
-            TextView sucess = findViewById(R.id.registersucess);
+            EditText nickname = findViewById(R.id.nickname);
+            TextView success = findViewById(R.id.registersucess);
 
             try {
                 usermanager.authenticate(username.getText().toString(), password.getText().toString());
+                // In authenticate, if True, the active user is already instantiate
             } catch (DuplicateAccountException e) {
                 username.setError("Sorry this username has been registered");
                 return;
@@ -82,8 +83,14 @@ public class RegisterActivity extends AppCompatActivity {
             if (password.getText().length() == 0) {
                 password.setError("Please fill in the password.");
             }
-            usermanager.getActiveUser().settarget();
-            sucess.setText("Register Successfully!");
+            //Check whether the user successfully registered
+            User curr_user = usermanager.getActiveUser();
+            if (curr_user != null){
+                curr_user.setNickName(nickname);
+                curr_user.setTarget(target);
+            }
+            success.setText("Register Successfully!");
+            target.setOnCheckedChangeListener(new Radio_check());
             Intent tmp = new Intent(this, LoginActivity.class);
             startActivity(tmp);
 
