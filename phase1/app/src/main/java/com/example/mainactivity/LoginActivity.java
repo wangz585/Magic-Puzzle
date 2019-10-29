14d79066becad83509bcce82728058e68e25f8f1
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2019-10-28 23:21:39 -0400

    Create loginActivity and update the xml design for login page.

---
+++ phase1/app/src/main/java/com/example/mainactivity/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import android.accounts.AccountsException;
+import android.accounts.AuthenticatorException;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * A UserManager.
+     */
+    private UserManager users;
+
+    /**
+     * The current player of the game.
+     */
+    public static User activePlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+        setSignInListener();
+    }
+
+    /**
+     *  Activate the signIn button.
+     */
+    @SuppressLint("SetTextI18n")
+    public void setSignInListener() {
+        Button signIn = findViewById(R.id.login);
+        signIn.setOnClickListener((v) -> {
+            TextView prompt_text = findViewById(R.id.prompt_text);
+            String userName = ((EditText)findViewById(R.id.username)).getText().toString();
+            String passWord = ((EditText)findViewById(R.id.password)).getText().toString();
+
+            try {
+                activePlayer = users.authenticate(userName, passWord);
+            }
+            catch (AuthenticatorException e){
+                prompt_text.setText("Password is incorrect.");
+                return;
+            }
+            catch (AccountsException e) {
+                prompt_text.setText("Username is not found.");
+                return;
+            }
+            prompt_text.setText("Sign In Successfully!");
+            Intent tmp = new Intent(this, MainMenuActivity.class);
+            startActivity(tmp);
+        });
+    }
+
+}
