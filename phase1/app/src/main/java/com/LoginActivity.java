a5ad4bc31496663794e8935913b45046fe449105
helenzzq <helenzhu0411@gmail.com>
2019-10-30 09:13:30 -0400

    Add back all the game file into the remote repo

---
+++ phase1/app/src/main/java/com/LoginActivity.java
@@ -0,0 +1,75 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import android.accounts.AccountsException;
+import android.accounts.AuthenticatorException;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * A UserManager.
+     */
+    private UserManager usermanager;
+
+    /**
+     * The current player of the game.
+     */
+    public static User activePlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+        setSignInListener();
+    }
+
+    /**
+     *  Activate the signIn button.
+     */
+    @SuppressLint("SetTextI18n")
+    public void setSignInListener() {
+        Button signIn = findViewById(R.id.login);
+        signIn.setOnClickListener((v) -> {
+            TextView prompt_text = findViewById(R.id.login_title);
+            EditText username = findViewById(R.id.username);
+            EditText password = findViewById(R.id.password);
+
+            try{
+                usermanager.authenticate(username.getText().toString(), password.getText().toString());
+            }
+            catch (AuthenticatorException e){
+                password.setError("Password is incorrect.");
+                return;
+            }
+            catch (AccountsException e) {
+                username.setError("Username is not found.");
+                return;
+            }
+            prompt_text.setText("Sign In Successfully!");
+            Intent tmp = new Intent(this, MainMenuActivity.class);
+            startActivity(tmp);
+        });
+    }
+
+
+    /**
+     * Activate the signUp button.
+     */
+    public void setRegisterListener(){
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener((v) -> {
+            Intent tmp = new Intent(this, RegisterActivity.class);
+            startActivity(tmp);
+        });
+
+    }
+
+}
---
+++ phase1/app/src/main/java/com/MainMenuActivity.java
@@ -0,0 +1,78 @@
+package com.example.mainactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        logOutListener();
+        Switch sbutton = findViewById(R.id.background);
+        onSwitchClicked(sbutton);
+        addGameButtonListeners();
+    }
+    private GameManager gameManager;
+
+    /**
+     * Activate the buttons for switching the background.
+     */
+    public void onSwitchClicked(View v){
+        //Is the switch on?
+        boolean on = ((Switch) v).isChecked();
+        ConstraintLayout layout = findViewById(R.id.mainlayout);
+        if(on)
+        {
+            layout.setBackground(ContextCompat.getDrawable(this, R.drawable.nightmountainbg));
+        }
+        else{
+            layout.setBackground(ContextCompat.getDrawable(this, R.drawable.background));
+        }
+
+        }
+    /**
+     * Activate the buttons for each game.
+     */
+    private void addGameButtonListeners() {
+        ImageView wordguess = findViewById(R.id.wordguess);
+        wordguess.setOnClickListener(v -> enterGame("guess_word"));
+
+        ImageView crazymatch = findViewById(R.id.crazymatch);
+        crazymatch.setOnClickListener(v -> enterGame("crazymatch"));
+
+        ImageView chess = findViewById(R.id.chess);
+        chess.setOnClickListener(v -> enterGame("chess"));
+    }
+
+    /**
+     * Switch to the correponding game.
+     * @param gameId a string referring to the type of game.
+     */
+    public void enterGame(String gameId){
+        //Make it more skillable that if you add new game into it, it will just access them by id
+        startActivities(new Intent(this, gameManager.getListOfGame().get(gameId)));
+    }
+
+    /**
+     * Activate the logout button.
+     */
+    public void logOutListener(){
+        Button signIn = findViewById(R.id.logout);
+        signIn.setOnClickListener((v) -> {
+            Intent tmp = new Intent(this, LoginActivity.class);
+            startActivity(tmp);
+        });
+    }
+
+
+
+}
