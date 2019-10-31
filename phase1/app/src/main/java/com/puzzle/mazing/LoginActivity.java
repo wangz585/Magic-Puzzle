88a731c0d4becf92b11d36b4093b18bd04a01549
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2019-10-30 23:18:48 -0400

    Comment out setRegisterListener(), to be implemented later. Changed minimum API level and target API level to 28.

--- phase1/app/src/main/java/com/puzzle/mazing/LoginActivity.java
+++ phase1/app/src/main/java/com/puzzle/mazing/LoginActivity.java
@@ -28,7 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 //        setSignInListener();
-        setRegisterListener();
+//        setRegisterListener();
     }
 //
 //    /**
