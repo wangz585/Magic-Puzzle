54601c8189b03c125fe4c49d7ca58392b3977a89
Jimmy <jimmylanbcn@gmail.com>
2019-10-31 11:41:46 -0400

    Add post with token option to Http

--- phase1/app/src/main/java/com/puzzle/mazing/Network/Http.java
+++ phase1/app/src/main/java/com/puzzle/mazing/Network/Http.java
@@ -10,6 +10,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class Http {
   private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
@@ -58,11 +59,49 @@ public class Http {
   }
 
 
+  /**
+   * Perform POST request with an authentication token
+   * @param url destination URL
+   * @param body body of request
+   * @param token authentication token to access protected resource
+   * @param done Callback Object used after request completes
+   */
+  public static void post(String url, JSONObject body, String token, Callback done) {
+    String bodyString = body.toString();
+    Request request = buildPostRequest(url, bodyString, token);
+
+    client.newCall(request).enqueue(done);
+  }
+
+
+  /**
+   * Perform POST request synchronously
+   * @param url destination URL
+   * @param body body of request
+   * @return Response object
+   * @throws IOException when request fail to deliver
+   */
   public static Response syncPost(String url, JSONObject body) throws IOException {
     String bodyString = body.toString();
     Request request = buildPostRequest(url, bodyString);
 
     return client.newCall(request).execute();
+  }
+
+
+  /**
+   * Perform POST request synchronously
+   * @param url destination URL
+   * @param body body of request
+   * @param token authentication token to access protected resource
+   * @return Response object
+   * @throws IOException when request fail to deliver
+   */
+  public static Response syncPost(String url, JSONObject body, String token) throws IOException {
+      String bodyString = body.toString();
+      Request request = buildPostRequest(url, bodyString, token);
+
+      return client.newCall(request).execute();
   }
 
 
@@ -72,5 +111,15 @@ public class Http {
             .url(url)
             .post(requestBody)
             .build();
+  }
+
+
+  private static Request buildPostRequest(String url, String bodyString, String token) {
+      RequestBody requestBody = RequestBody.create(JSON, bodyString);
+      return new Request.Builder()
+              .url(url)
+              .post(requestBody)
+              .addHeader("authentication", token)
+              .build();
   }
 }
