da5ee0b501b7d71f761543099987aee2ca4918a9
YolandaBeCool <zyq666@yandex.com>
2019-10-31 00:08:51 -0400

    Add a gameRecord and a gameState class

---
+++ phase1/app/src/main/java/com/puzzle/mazing/Games/GameState.java
@@ -0,0 +1,54 @@
+package com.puzzle.mazing.Games;
+
+import com.puzzle.mazing.Network.Http;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class GameState {
+    private HashMap states;
+//    private Game currentGame;
+//    private User currentUser;
+
+//    public GameState(Game currentGame, User currentUser) {
+//        this.states = new HashMap<String, Integer> ();
+//        this.currentGame = currentGame;
+//        this.currentUser = currentUser;
+//    }
+
+
+//    public void uploadState(){
+//        // this should upload the state to the server
+//        JSONObject state_to_json = new JSONObject();
+//        try {
+//            state_to_json.put("GameStateForUser" + currentUser.getUserID() + currentGame.getID(), states);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+//        Http.post("https://apis.puzzlemazing.online/test/post", state_to_json, new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                final String res = response.body().string();
+//            }
+//        });
+
+//    }
+//
+//    public void setStates(String key, int value){
+//        states.put(key, value);
+//    }
+//    public HashMap getStates(){
+//        return states;
+//    }
+//    public boolean checkCurrentState(){
+//    }
+}
