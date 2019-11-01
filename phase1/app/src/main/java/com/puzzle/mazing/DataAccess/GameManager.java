0a00e2971dadb796be3f2bd8c2b7cf2b4d770dd2
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2019-10-31 22:02:18 -0400

    Add GameManager.java and implement GameManager.

---
+++ phase1/app/src/main/java/com/puzzle/mazing/DataAccess/GameManager.java
@@ -0,0 +1,40 @@
+package com.puzzle.mazing.DataAccess;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameManager {
+    /*
+    private List<Game> myGames;
+
+    GameManager() {
+        myGames = new ArrayList<Game>();
+    }
+
+    void addGame(Game newGame) {
+        myGames.add(newGame);
+    }
+
+    void removeGame(int id) {
+        myGames.remove(getGame(id));
+    }
+
+    Game getGame(int id) {
+        for (Game game: myGames) {
+            if (game.getId() == id) {
+                return game;
+            }
+        }
+        return null;
+    }
+
+    List<Game> getAllGames() {
+        return myGames;
+    }
+
+    boolean isFirstPlay(int id) {
+        return getGame(id).getIsFirstPlay();
+    }
+
+     */
+}
+
