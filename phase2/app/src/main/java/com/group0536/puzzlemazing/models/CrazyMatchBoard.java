01f6769f5af35ba79750998432f28ca241c82261
YolandaBeCool <zyq666@yandex.com>
2019-11-19 10:59:48 -0500

    Create a model for CrazyMatchBoard

---
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/models/CrazyMatchBoard.java
@@ -0,0 +1,36 @@
+package com.group0536.puzzlemazing.models;
+//
+//public class CrazyMatchBoard {
+//    private List<List<Animal>> animals;
+//
+//    /**
+//     *
+//     * @param animals a map stores all animals in the map
+//     */
+//    public CrazyMatchBoard(List<List<Animal>> animals){
+//        animals = animals;
+//    }
+//
+//    /**
+//     *
+//     * @param row
+//     * @param col
+//     * @return the animal at (row, col) in the map
+//     */
+//    public Animal getAnimal(int row, int col){
+//        return animals.get(row).get(col);
+//    }
+//
+//    /**
+//     *
+//     * @param animal
+//     * Add an new animal into the map at location (row, col)
+//     */
+//    public void insertAnimal(Animal animal){
+//        int[] position = animal.getPosition();
+//        int row = position.get(0);
+//        int col = position.get(1);
+//
+//        animals.get(row).set(col, animal);
+//    }
+//}
