bad0f8c381c506ef5eb6a9adce6323c7ce18a4d9
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2019-11-20 03:25:08 -0500

    Add crazy match level one activity

---
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/views/crazymatch/GameActivityLevelOne.java
@@ -0,0 +1,19 @@
+package com.group0536.puzzlemazing.views.crazymatch;
+
+import android.os.Bundle;
+
+import com.group0536.puzzlemazing.R;
+import com.group0536.puzzlemazing.views.FluxActivity;
+
+public class GameActivityLevelOne extends FluxActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crazy_match_level_one);
+    }
+
+    @Override
+    protected void initFluxComponents() {
+
+    }
+}
