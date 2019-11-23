f03b666cd225b9da377f9f75d6dfe0f19464f66b
Zhenyu WANG <zhenyu.wang@mail.utoronto.ca>
2019-11-23 01:58:18 -0500

    Set up busyworker.SelectLevelActivity

---
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/views/busyworker/SelectLevelActivity.java
@@ -0,0 +1,64 @@
+package com.group0536.puzzlemazing.views.busyworker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.group0536.puzzlemazing.R;
+import com.group0536.puzzlemazing.actions.busyworker.BusyWorkerActionCreator;
+import com.group0536.puzzlemazing.stores.busyworker.BusyWorkerStore;
+import com.group0536.puzzlemazing.views.FluxActivity;
+import com.group0536.puzzlemazing.views.crazymatch.GameActivityLevelOne;
+
+public class SelectLevelActivity extends FluxActivity {
+
+    private BusyWorkerActionCreator actionCreator;
+    private BusyWorkerStore store;
+    Button btnLevel1;
+    Button btnLevel2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crazy_match_select_level);
+        btnLevel1 = findViewById(getResources()
+                .getIdentifier("btn_level_1", "id", getPackageName()));
+        btnLevel2 = findViewById(getResources()
+                .getIdentifier("btn_level_2", "id", getPackageName()));
+        btnLevel1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                actionCreator.initMap(1);
+                Intent intent = new Intent(SelectLevelActivity.this, GameActivityLevelOne.class);
+                startActivity(intent);
+            }
+        });
+        btnLevel2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                actionCreator.initMap(2);
+                Intent intent = new Intent(SelectLevelActivity.this, GameActivityLevelOne.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void initFluxComponents() {
+        store = BusyWorkerStore.getInstance(dispatcher);
+        actionCreator = new BusyWorkerActionCreator(dispatcher);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerStore(store);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        registerStore(store);
+    }
+}
