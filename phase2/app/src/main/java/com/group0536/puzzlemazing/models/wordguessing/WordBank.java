f4cb9bd0dcace104aaebf7ab4533b9943d5659d4
YolandaBeCool <zyq666@yandex.com>
2019-11-24 13:58:07 -0500

    Create model WordBank

---
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/models/wordguessing/WordBank.java
@@ -0,0 +1,34 @@
+package com.group0536.puzzlemazing.models.wordguessing;
+
+import java.util.List;
+import java.util.Random;
+
+public class WordBank {
+    private List<Word> vocabulary;
+
+    public WordBank(List<Word> vocabulary) {
+        this.vocabulary = vocabulary;
+    }
+
+    public List<Word> getVocabulary() {
+        return vocabulary;
+    }
+
+    /**
+     * Get a word that is not been guessed by the player
+     * @return a word for the player to guess next
+     */
+    public Word getARandomWord(){
+        Random rand = new Random();
+        boolean isNewWord = false;
+        Word newWordForGuessing = null;
+        while(!isNewWord){
+            newWordForGuessing = vocabulary.get(rand.nextInt(vocabulary.size()));
+            if (!newWordForGuessing.isGuessed()){
+                isNewWord = true;
+
+            }
+        }
+        return newWordForGuessing;
+    }
+}
