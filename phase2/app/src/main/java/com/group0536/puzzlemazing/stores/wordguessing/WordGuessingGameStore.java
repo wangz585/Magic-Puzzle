b70ae1af3faff9914cf05406243957d70e78d1c6
YolandaBeCool <zyq666@yandex.com>
2019-11-24 18:06:05 -0500

    Add one word list for a new level and finish initializing word bank

--- phase2/app/src/main/java/com/group0536/puzzlemazing/stores/wordguessing/WordGuessingGameStore.java
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/stores/wordguessing/WordGuessingGameStore.java
@@ -1,5 +1,7 @@
 package com.group0536.puzzlemazing.stores.wordguessing;
 
+import android.content.Context;
+
 import com.group0536.puzzlemazing.actions.Action;
 import com.group0536.puzzlemazing.actions.wordguessing.WordGuessingActions;
 import com.group0536.puzzlemazing.dispatcher.Dispatcher;
@@ -11,6 +13,10 @@ import com.group0536.puzzlemazing.stores.Store;
 import com.group0536.puzzlemazing.stores.StoreChangeEvent;
 import com.squareup.otto.Subscribe;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -97,7 +103,8 @@ public class WordGuessingGameStore extends Store implements WordGuessingActions 
                 postChange();
                 break;
             case INITIALIZE_WORDBANK:
-                initializeWordBank((int) action.getPayloadEntry("level"));
+                initializeWordBank((int) action.getPayloadEntry("level"),
+                        (Context) action.getPayloadEntry("context"));
                 postChange();
                 break;
         }
@@ -106,16 +113,63 @@ public class WordGuessingGameStore extends Store implements WordGuessingActions 
     /**
      * Initialize a word bank based on the game level
      * @param level
+     * @param context
      */
-    private void initializeWordBank(int level) {
-        if (level == 1){
+    private void initializeWordBank(int level, Context context) {
+            wordBank = new WordBank(getWordList(level, context));
+        }
 
+    private List<Word> getWordList(int level, Context context) {
+        List<Word> wordsInBank = new ArrayList<>();
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    context.getAssets().open("wordGuessingLevel"+level+".txt")));
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String[] words = line.split("--");
+                List<List<Character>> splicedWord = splitWord(words[0], level);
+                Word newWord = new Word(words[0],
+                        splicedWord.get(0),
+                        splicedWord.get(1),
+                        words[1]);
+                wordsInBank.add(newWord);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return wordsInBank;
+    }
+
+    /**
+     * Split a word into two part: missing chars and shown chars to the user
+      * @param word
+     * @return a list of two list of characters
+     */
+    private List<List<Character>> splitWord(String word, int level) {
+        List<List<Character>> splicedWord = new ArrayList<>();
+        List<Character> shownChars = new ArrayList<>();
+        List<Character> missingChars = new ArrayList<>();
+
+        int wordLength;
+        if(level == 1){
+            wordLength = 6;
         }
         else{
-                // if level is 2
-            }
-
+            wordLength = 8;
         }
+
+        for(int i = 0; i < wordLength; i++){
+            if(i % 2 == 0){
+                shownChars.add(word.charAt(i));
+            }
+            else{
+                missingChars.add(word.charAt(i));
+            }
+        }
+        splicedWord.add(shownChars);
+        splicedWord.add(missingChars);
+        return splicedWord;
+    }
 
     /**
      *  Check if the user guesses the word correctly
