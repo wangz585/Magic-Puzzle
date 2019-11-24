b48c0784a0f391349bcea85cb53f9a8d3f36b1f2
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2019-11-24 15:31:11 -0500

    Update word guessing game layout

--- phase2/app/src/main/java/com/group0536/puzzlemazing/views/wordguessing/GameActivity.java
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/views/wordguessing/GameActivity.java
@@ -1,8 +1,20 @@
 package com.group0536.puzzlemazing.views.wordguessing;
 
+import android.os.Bundle;
+
+import com.group0536.puzzlemazing.R;
 import com.group0536.puzzlemazing.views.FluxActivity;
 
 public class GameActivity extends FluxActivity {
+
+    public GameActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_guessing_level_one);
+    }
 
     @Override
     protected void initFluxComponents() {
