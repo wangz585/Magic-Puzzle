cb3b3c34f5c10dd43be568969c461f4f748a2094
YolandaBeCool <zyq666@yandex.com>
2019-11-24 15:19:25 -0500

    Update onAction for store

--- phase2/app/src/main/java/com/group0536/puzzlemazing/stores/wordguessing/WordGuessingGameStore.java
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/stores/wordguessing/WordGuessingGameStore.java
@@ -5,15 +5,22 @@ import com.group0536.puzzlemazing.actions.wordguessing.WordGuessingActions;
 import com.group0536.puzzlemazing.dispatcher.Dispatcher;
 import com.group0536.puzzlemazing.models.User;
 import com.group0536.puzzlemazing.models.wordguessing.ScoreCalculator;
+import com.group0536.puzzlemazing.models.wordguessing.Word;
+import com.group0536.puzzlemazing.models.wordguessing.WordBank;
 import com.group0536.puzzlemazing.stores.Store;
 import com.group0536.puzzlemazing.stores.StoreChangeEvent;
 import com.squareup.otto.Subscribe;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class WordGuessingGameStore extends Store implements WordGuessingActions {
     private WordGuessingInitController initController = new WordGuessingInitController();
     private User player;
+    private WordBank wordBank;
+    private Word currentWord;
+    private boolean gameOver;
+    private int score;
     private ScoreCalculator scoreCalculator;
     private static com.group0536.puzzlemazing.stores.wordguessing.WordGuessingGameStore instance;
 
@@ -21,6 +28,11 @@ public class WordGuessingGameStore extends Store implements WordGuessingActions 
         super(dispatcher);
         // TODO: pass in a player
 //        this.player = player;
+        this.currentWord = null;
+        this.gameOver = false;
+        this.wordBank = null;
+        this.score = 0;
+//        this.scoreCalculator = null;
     }
 
     /**
@@ -41,18 +53,102 @@ public class WordGuessingGameStore extends Store implements WordGuessingActions 
         return new WordGuessingChangeEvent();
     }
 
+    public void setGameOver() {
+        gameOver = true;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
     @Override
     @Subscribe
     public void onAction(Action action) {
         switch (action.getType()) {
-            case SHOW_HINT:
+//            case TIME_OVER:
+//                setGameOver();
+            case START_GAME:
+                Word newWord = getANewWord();
+                setCurrentWord(newWord);
+                postChange();
                 break;
-            case CHECK:
+            case SUBMIT_ANSWER:
+                List<Character> userInput = (List<Character>) action.getPayloadEntry("User Input");
+                if(isCorrect(userInput)){
+                    // if the answer is correct
+                    // updateScore();
+                    // if the game is finished
+                    if(isGameOver()){
+                        // time is out, the user cannot longer click the submit button
+
+                    }
+                    else{
+                        // there is time left
+                        // clear the user input
+                        // give a new word
+
+                    }
+                }
+                else{
+                    // if the answer is not correct
+                    // clear over the user input
+
+                }
+                postChange();
                 break;
-            case SKIP:
-                break;
+//            case INITIALIZE_WORDBANK:
+//                initializeWordBank((int) action.getPayloadEntry("level"));
+//                postChange();
+//                break;
+        }
+    }
+
+    /**
+     * Initialize a word bank based on the game level
+     * @param level
+     */
+    private void initializeWordBank(int level) {
+        if (level == 1){
 
         }
+        else{
+            // if level is 2
+        }
+    }
+
+    /**
+     *  Check if the user guesses the word correctly
+     * @param userInput user's input
+     * @return true if the user guesses the word correctly
+     */
+    private boolean isCorrect(List<Character> userInput) {
+        List<Character> missingChars = currentWord.getMissingChars();
+        if(userInput.size() != missingChars.size()){
+            return false;
+        }
+        for (int i = 0; i < userInput.size(); i++){
+            if (!userInput.get(i).equals(missingChars.get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Set the current word for guessing so that the store knows which word the player is guessing
+     * @param newWord
+     */
+    private void setCurrentWord(Word newWord) {
+        this.currentWord = newWord;
+    }
+
+    /**
+     * Get a new word that is never been guessed from the word bank for the player to guess
+     * @return a new word
+     */
+    private Word getANewWord() {
+        Word newWord = wordBank.getARandomWord();
+        return newWord;
     }
 
     public int getContentView(int level) {
