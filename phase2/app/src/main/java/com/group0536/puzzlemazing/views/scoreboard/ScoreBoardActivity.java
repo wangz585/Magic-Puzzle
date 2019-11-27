f7bc9aada84ffb5451dacf34bf50578642407f1b
YolandaBeCool <zyq666@yandex.com>
2019-11-27 18:06:43 -0500

    Register ScoreBoardActivity

---
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/views/scoreboard/ScoreBoardActivity.java
@@ -0,0 +1,11 @@
+package com.group0536.puzzlemazing.views.scoreboard;
+
+import com.group0536.puzzlemazing.views.FluxActivity;
+
+public class ScoreBoardActivity extends FluxActivity {
+    @Override
+    protected void initFluxComponents() {
+
+    }
+
+}
