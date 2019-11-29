08fc71c683ad340a6178de417b5a9f4372aea433
lianzhim <zhiming.lian@mail.utoronto.ca>
2019-11-29 14:22:14 -0500

    fix flaws and add comment to BitMap Class

--- phase2/app/src/main/java/com/group0536/puzzlemazing/models/busyworker/BitMap.java
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/models/busyworker/BitMap.java
@@ -13,6 +13,10 @@ public class BitMap {
     private static Bitmap WallBitmap;
     private static Bitmap FlagBitmap;
 
+    /**
+     * Initialize all images that is needed in the game
+     * @param res the place where all images are stored
+     */
     public static void initBitmaps(Resources res) {
         WorkerBitmap = BitmapFactory.decodeResource(res, R.drawable.worker_worker);
         BoxBitmap = BitmapFactory.decodeResource(res, R.drawable.worker_skull);
@@ -20,18 +24,38 @@ public class BitMap {
         FlagBitmap = BitmapFactory.decodeResource(res, R.drawable.worker_tomb);
     }
 
+    /**
+     * Get the image of the worker
+     *
+     * @return bitmap of the worker
+     */
     public static Bitmap getWorkerBitmap() {
         return WorkerBitmap;
     }
 
+    /**
+     * Get the image of the box
+     *
+     * @return bitmap of the box
+     */
     public static Bitmap getBoxBitmap() {
         return BoxBitmap;
     }
 
+    /**
+     * Get the image of the wall
+     *
+     * @return bitmap of the wall
+     */
     public static Bitmap getWallBitmap() {
         return WallBitmap;
     }
 
+    /**
+     * Get the image of the flag
+     *
+     * @return bitmap of the flag
+     */
     public static Bitmap getFlagBitmap() {
         return FlagBitmap;
     }
