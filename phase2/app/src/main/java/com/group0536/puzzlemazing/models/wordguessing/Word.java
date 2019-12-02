c211c6de35af4a704d058fe3a56aaf8e93c3439c
YolandaBeCool <zyq666@yandex.com>
2019-12-01 19:35:52 -0500

    Add docstring for model Word

--- phase2/app/src/main/java/com/group0536/puzzlemazing/models/wordguessing/Word.java
+++ phase2/app/src/main/java/com/group0536/puzzlemazing/models/wordguessing/Word.java
@@ -10,7 +10,13 @@ public class Word {
     private List<Character> initialState;
     private List<Character> currentState;
 
-
+    /**
+     * Initialize a Word for word guessing game
+     *
+     * @param spelling     the spelling for the word
+     * @param hint         the hint of the word
+     * @param initialState a list of characters for the word
+     */
     public Word(String spelling, String hint, List<Character> initialState) {
         this.spelling = spelling;
         this.hint = hint;
@@ -18,34 +24,65 @@ public class Word {
         this.currentState = new ArrayList<>(initialState);
     }
 
+    /**
+     * Get the spelling of the word
+     *
+     * @return spelling
+     */
     public String getSpelling() {
         return spelling;
     }
 
+    /**
+     * Get whether the word is guessed
+     *
+     * @return guessed
+     */
     public boolean isGuessed() {
         return guessed;
     }
 
+    /**
+     * Set the word is guessed or not
+     *
+     * @param guessed
+     */
     public void setGuessed(boolean guessed) {
         this.guessed = guessed;
     }
 
+    /**
+     * Get the hint of the word
+     *
+     * @return hint
+     */
     public String getHint() {
         return hint;
     }
 
-    public void setCharAt(int index, char character) {
-        currentState.set(index, character);
-    }
-
+    /**
+     * Return the list of characters of a word
+     *
+     * @return initialState
+     */
     public List<Character> getInitialState() {
         return initialState;
     }
 
+    /**
+     * Return the list of characters of a word
+     *
+     * @return currentState
+     */
     public List<Character> getCurrentState() {
         return currentState;
     }
 
+    /**
+     * Set the current state
+     *
+     * @param newState a new list of characters
+     */
     public void setCurrentState(List<Character> newState) {
         currentState = newState;
     }
